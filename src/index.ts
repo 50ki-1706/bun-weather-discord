bded9ff5f10bd86cc43d8e7aa05b13acb64729af
tiyo5151 <s1F102400224@iniad.org>
2025-05-15 21:15:14 +0900

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { ActivityType, Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { CommandInteraction, ChatInputCommandInteraction } from 'discord.js';
 
 // create a new Client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    // GatewayIntentBits.GuildMembers,
+    // GatewayIntentBits.GuildMessages,
+    // GatewayIntentBits.MessageContent,
+    // GatewayIntentBits.GuildPresences,
+  ],
+});
 
 // listen for the client to be ready
 client.once(Events.ClientReady, (c) => {
@@ -12,4 +23,54 @@ client.once(Events.ClientReady, (c) => {
 client.login(process.env.DISCORD_TOKEN).catch((err) => {
   console.error('Failed to login:', err);
   process.exit(1);
+});
+
+client.commands = new Collection<string, (interaction: CommandInteraction) => Promise<void>>();
+
+const folderPath = path.join(__dirname, 'commands');
+const commandFolderPath = path.join(folderPath, 'util');
+const commandFolders = fs.readdirSync(commandFolderPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(commandFolderPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.ts'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command.execute);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command(interaction as ChatInputCommandInteraction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error executing this command!',
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error executing this command!',
+        ephemeral: true,
+      });
+    }
+  }
 });
